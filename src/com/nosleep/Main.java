02b41a3e381d9e0ad23851938480802c69fa6eab
kuanghc <609779612@qq.com>
2025-08-12 20:45:00 +0800

    modify

--- src/com/nosleep/Main.java
+++ src/com/nosleep/Main.java
@@ -1,6 +1,7 @@
 package com.nosleep;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class Main {
 
@@ -9,9 +10,15 @@ public class Main {
             while (true) {
                 try {
                     Robot robot = new Robot();
+//                    robot.keyPress(KeyEvent.VK_2);
+//                    robot.keyRelease(KeyEvent.VK_2);
+//                    robot.keyPress(KeyEvent.VK_1);
+//                    robot.keyRelease(KeyEvent.VK_1);
+//                    robot.keyPress(KeyEvent.VK_SPACE);
+//                    robot.keyRelease(KeyEvent.VK_SPACE);
                     robot.mouseMove(getMouseX() + 1, getMouseY() + 1);
                     robot.mouseMove(getMouseX() - 1, getMouseY() - 1);
-                    Thread.sleep(60 * 1000);
+                    Thread.sleep(10 * 1000);
                 } catch (InterruptedException | AWTException e) {
                     e.printStackTrace();
                 }
