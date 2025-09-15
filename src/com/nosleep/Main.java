4314dd377232ed48e11e445dd68f96a403b28d0a
kuanghc <609779612@qq.com>
2025-09-15 20:11:07 +0800

    modify

--- src/com/nosleep/Main.java
+++ src/com/nosleep/Main.java
@@ -11,24 +11,29 @@ public class Main {
             while (true) {
                 try {
                     Robot robot = new Robot();
+//                    robot.mousePress(MouseEvent.BUTTON3_DOWN_MASK);
+//                    robot.mouseRelease(MouseEvent.BUTTON3_DOWN_MASK);
+//                    Thread.sleep(3000);
 //                    robot.keyPress(KeyEvent.VK_2);
 //                    robot.keyRelease(KeyEvent.VK_2);
 //                    robot.keyPress(KeyEvent.VK_1);
 //                    robot.keyRelease(KeyEvent.VK_1);
                     robot.keyPress(KeyEvent.VK_SPACE);
                     robot.keyRelease(KeyEvent.VK_SPACE);
-//                    robot.mouseMove(getMouseX() + 1, getMouseY() + 1);
-//                    robot.mouseMove(getMouseX() - 1, getMouseY() - 1);
-                    // 创建一个随机数生成器
+////                    robot.mouseMove(getMouseX() + 1, getMouseY() + 1);
+////                    robot.mouseMove(getMouseX() - 1, getMouseY() - 1);
+
+//                    // 创建一个随机数生成器
                     Random random = new Random();
-
-                    // 生成 0 到 60,000 毫秒之间的随机数
+//
+//                    // 生成 0 到 60,000 毫秒之间的随机数
                     int randomDelay = random.nextInt(60_000 * 3); // 0 到 59999 毫秒
-
-                    System.out.println("Sleeping for " + randomDelay + " milliseconds.");
                     Thread.sleep(randomDelay);
-                } catch (InterruptedException | AWTException e) {
+                    System.out.println("Sleeping for " + randomDelay + " milliseconds.");
+                } catch (AWTException e) {
                     e.printStackTrace();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
                 }
             }
         }).start();
